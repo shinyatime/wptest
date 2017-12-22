faed363181f846cc5f5282c8e26615e129e6d049
shinyatime <shinyatan@sina.com>
2017-12-22 17:45:54 +0800

    webpack postcss实验

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,52 @@ var path = require('path');
 var htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/script/main.js',
+    entry: './src/app.js',
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: 'js/[name].[chunkhash].js'
+        filename: 'js/[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: path.resolve(__dirname, './node_modules'),
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        "presets": ['env']
+                    }
+                }]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: [
+                                require('autoprefixer')()
+                            ]
+                        }
+                    }
+                ]
+            }
+        ]
     },
     plugins: [
         new htmlWebpackPlugin({
-            filename: 'index.[chunkhash].html',
+            filename: 'index.html',
             template: 'index.html',
-            inject: 'head'
+            inject: 'hody'
         })
     ]
 }
